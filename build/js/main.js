c97855dbcb3bbc8b9d095bb33371031f06ac936d
Ilia Pohorielov <ilia.pohorielov@gmail.com>
2017-12-06 17:32:50 +0200

    style footer and header + mobile

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,7 @@
+/* begin sticky-menu */
 var  menu = $(".js-sticky-menu"),
     h_menu = $(".js-sticky-menu").height();
+
 $(window).scroll(function() {
     if ($(this).scrollTop() <= h_menu) {
         menu.css('top', '-' + $(this).scrollTop() + 'px');
@@ -10,4 +12,30 @@ $(window).scroll(function() {
     } else {
         menu.removeClass('sticky');
     }
+});
+/* end sticky-menu */
+
+/* begin accordion footer */
+$(".js-nav-title").click(function () {
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        panel.classList.remove("show");
+        $(this).removeClass('rotate');
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "%";
+        panel.classList.add("show");
+        $(this).addClass('rotate');
+    }
+});
+/* end accordion footer */
+
+$('.js-mobile-menu').on('click', function () {
+   $(this).toggleClass('active');
+   $('.header-container').toggleClass('open');
+});
+
+$('.js-menu-close').on('click', function () {
+    $('.js-mobile-menu').removeClass('active');
+   $('.header-container').removeClass('open');
 });

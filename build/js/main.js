eab23615fd6c5e6fdd432bf1919666e8f08ebbc6
Ilia Pohorielov <ilia.pohorielov@gmail.com>
2017-12-07 19:03:32 +0200

    style homepage

--- build/js/main.js
+++ build/js/main.js
@@ -29,7 +29,7 @@ $(".js-nav-title").click(function () {
     }
 });
 /* end accordion footer */
-
+/* begin mobile menu */
 $('.js-mobile-menu').on('click', function () {
    $(this).toggleClass('active');
    $('.header-container').toggleClass('open');
@@ -39,3 +39,36 @@ $('.js-menu-close').on('click', function () {
     $('.js-mobile-menu').removeClass('active');
    $('.header-container').removeClass('open');
 });
+/* end mobile menu */
+/* begin height block */
+function heightEL() {
+    var elH = document.getElementsByClassName("height");
+    var maxHeight = 0;
+    for (var i = 0; i < elH.length; ++i) {
+        elH[i].style.height = "";
+        if (maxHeight < elH[i].clientHeight) {
+            maxHeight = elH[i].clientHeight;
+        }
+    }
+    for (var i = 0; i < elH.length; ++i) {
+        elH[i].style.height = maxHeight + "px";
+    }
+}
+heightEL();
+if ($(window).width() > 767) {
+    $(window).on('resize', function () {
+        heightEL();
+    });
+    $(window).on('load', function () {
+        heightEL();
+    });
+}
+/*end height block */
+/*begin hover choosing*/
+$('.service-item').on('mouseenter',function () {
+    $(this).addClass('hover');
+});
+$('.service-item').on('mouseleave',function () {
+    $(this).removeClass('hover');
+});
+/*end hover choosing*/
